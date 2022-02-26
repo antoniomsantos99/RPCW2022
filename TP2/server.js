9c64b60c65426c22bbae375c49aac9960b172aa0
António Santos <antoniomsantos99@gmail.com>
2022-02-26 21:43:06 +0000

    TPC2 - update server

--- TP2/server.js
+++ TP2/server.js
@@ -2,9 +2,12 @@ var http = require('http')
 var fs = require('fs')
 
 http.createServer(function (req, res) {
-    var myurl = req.url
-    console.log(myurl)
-    fs.readFile('./filmes/f' + myurl + '.html', function (err, data) {
+    var myurl = req.url.substring(1)
+    var path = myurl.substring(start=0,end=myurl.lastIndexOf("/"))
+    var file = myurl.substring(start=myurl.lastIndexOf("/"))
+    console.log(path)
+    console.log(file)
+    fs.readFile(path, function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         if (err) {
             res.write("<p>Erro na leitura de ficheiro...</p>")
